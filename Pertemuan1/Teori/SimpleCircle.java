80c3e035663a82586412d7f7ad6522854ba2a148
radikjr <80373803+radikjr@users.noreply.github.com>
2021-03-14 15:56:44 +0700

    Add files via upload

---
+++ Pertemuan1/Teori/SimpleCircle.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author radik
+ */
+public class SimpleCircle {
+    
+    double radius=1;
+   
+    
+    SimpleCircle() {
+    }
+    
+    SimpleCircle(double newRadius) {
+        radius = newRadius;
+    }
+    
+    double getArea() {
+        return radius * radius * Math.PI;
+    }
+    
+    double setRadius(double newRadius){
+        radius = newRadius;
+        return 0;
+    }
+}
+
+
