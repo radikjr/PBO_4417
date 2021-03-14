a48941200abf51dbea4575f2146065993547ef0a
radikjr <80373803+radikjr@users.noreply.github.com>
2021-03-14 21:12:48 +0700

    Add files via upload

---
+++ Pertemuan2/Teori/Matematika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author radik
+ */
+class Matematika{
+      int hasil;
+
+      public void tambah(int a, int b){
+            hasil = a + b;
+            System.out.println("Pertambahan :" + a + "+" + b + "=" + hasil);
+      }
+      public void kurang(int a, int b){
+            hasil = a - b;
+            System.out.println("Pengurangan :" + a + "-" + b + "=" + hasil);
+      }
+      public void kali(int a, int b){
+            hasil = a * b;
+            System.out.println("Perkalian   :" + a + "*" + b + "=" + hasil);
+      }
+      public void bagi(int a, int b){
+            hasil = a / b;
+            System.out.println("Pembagian  :" + a + "/" + b + "=" + hasil);
+      }
+}
---
+++ Pertemuan2/Teori/MatematikaDemo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author radik
+ */
+public class MatematikaDemo {
+      public static void main(String[] args){
+      Matematika mtk = new Matematika();
+      mtk.tambah(50, 10);
+      mtk.kurang(20, 10);
+      mtk.kali(5, 20);
+      mtk.bagi(20, 5);
+      }
+}
